0706d25f598496f9add8ecfe1665cd74b4f2ddde
Farnaz Yousefi <farnaz.usefi@gmail.com>
2019-10-13 18:40:59 -0400

    fixing send message

--- mcast.c
+++ mcast.c
@@ -743,7 +743,7 @@ void sendMessage(enum TYPE type, char *dp, int payloadSize) {
 	log_trace("sending:");
 	int j;
 	for(j = 0; j < 12; j++)
-		printf("%02X", message[j]);
+		printf("oo%02X", message[j]);
 	log_trace("\n");
 	sendto(currentSession.sendingSocket, &message, payloadSize+12, 0,
 			(struct sockaddr*) &currentSession.sendAddr,
